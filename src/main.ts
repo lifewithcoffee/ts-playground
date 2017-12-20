1bfdcbe3c06f0d094a26f63649c940f2d0b7492a
Ron <cynosure.veda@gmail.com>
2017-12-20 17:12:33 +1100

    update logic code

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,23 @@
+import {Factorial} from './factorial';
+
+const argc = process.argv.splice(2);
+
+if(argc.length != 1){
+    console.error(`Invalid number of arguments ${argc}`);
+    process.exit(1);
+}
+
+const inputNumber = argc[0];
+
+if(! /^\d+$/.test(inputNumber)) {
+    console.error(`Invalid input ${inputNumber}`);
+    process.exit(1);
+}
+
 export class Main{
-    static log(val:any){
-        console.log(val);
+    static main(){
+        const fact = new Factorial(+inputNumber);
+        console.log(`Factorial of ${inputNumber} is ${fact.getFactorial()}`);
     }
 }
-Main.log('Hello World');
+Main.main();
