85aed789f9524e112c6e168ff8352ef3776292cc
Ron <cynosure.veda@gmail.com>
2017-12-21 00:45:05 +1100

    improve cmd argument parsing

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,56 @@
 import {Factorial} from './factorial';
 import {FileNavigator} from './fileNavigator';
 
-export class Main{
-    static main(){
-        const argc = process.argv.splice(2);
+class Cmds {
+    help() {
+        console.log("Listing command methods:");
 
-        if(argc.length != 1){
-            console.error(`Invalid number of arguments ${argc}`);
-            process.exit(1);
-        }
-
-        var arg = argc[0];
-
-        // const fact = new Factorial(this.getNumberFromArgs(arg));
-        // console.log(`Factorial of ${arg} is ${fact.getFactorial()}`);
-
-        new FileNavigator().listFiles(arg);
+        var list_without_constructor = Object.getOwnPropertyNames(Object.getPrototypeOf(this)).splice(1);
+        console.log(list_without_constructor);
     }
+    
+    factorial() {
+        var arg = argc[1];
+        console.log(`Input number is ${arg}`);
 
-    static getNumberFromArgs(arg:string):number{
         if(! /^\d+$/.test(arg)) {
             console.error(`Invalid input ${arg}`);
             process.exit(1);
         }
 
-        return +arg;
+        const fact = new Factorial(+arg);
+        console.log(`Factorial of ${arg} is ${fact.getFactorial()}`);
+
+    }
+
+    listFiles(){
+        var arg = argc[1];
+        new FileNavigator().listFiles(arg);
     }
 }
 
-Main.main();
+export class Main{
+    static main(argc:string[]){
+
+        if(argc.length < 1){
+            console.error(`Invalid number of arguments ${argc}`);
+            process.exit(1);
+        }
+        
+        var arg = argc[0];
+
+        //this[arg]();
+        var cmd = new Cmds();
+
+        if(cmd[arg] == null) {
+            console.log("There in no such command: ${arg}; Try command 'help' for more info.");
+        }else{
+            console.log(`Executing command: ${arg}`);
+            console.log("-----------------------------");
+            cmd[arg]();
+        }
+    }
+}
+
+const argc = process.argv.splice(2);
+Main.main(argc);
